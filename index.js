2f6ce3a30c2cbff8c132f30b711f2b969bb33449
Matthew Holcombe <matthewholcombe@Matthews-MacBook-Pro-8.local>
2022-09-30 15:40:36 -0400

    finlized, with pretty code

--- index.js
+++ index.js
@@ -7,31 +7,27 @@ setUpTabs = () => {
       const sideBar = button.parentElement; //div sidebar
       const tabsContainer = sideBar.parentElement; //div tabs
       const tabsNumber = button.dataset.forTab; //data-for-tab in button class
-      const tabToActivate = tabsContainer.querySelector(`.tabs__content[data-tab="${tabsNumber}"]`); //tab content
-      // console.log(tabToActivate)
+      const tabToActivate = tabsContainer.querySelector(`.tabs__content[data-tab="${tabsNumber}"]`); //tab content by data-for-tabs number
       console.log(tabsContainer)
       // console.log(sideBar);
       // console.log(tabsContainer);
       // console.log(tabsNumber);
       // console.log(tabToActivate);
 
-      sideBar.querySelectorAll('.tabs__button').forEach(button => {
+      sideBar.querySelectorAll('.tabs__button').forEach(button => { //deselects all side tabs
         button.classList.remove('tabs__button--active');
       });
 
-      tabsContainer.querySelectorAll('.tabs__content').forEach(tab => {
+      tabsContainer.querySelectorAll('.tabs__content').forEach(tab => { // hides content for each tab
         tab.classList.remove('tabs__content--active');
-
       });
 
-      button.classList.add('tabs__button--active');
-      console.log(tabToActivate)
+      button.classList.add('tabs__button--active'); // adds the activated content to tab button and content page
       tabToActivate.classList.add('tabs__content--active');
-
     })
   })
 }
-  document.addEventListener('DOMContentLoaded', () => {
+  document.addEventListener('DOMContentLoaded', () => { // triggers set up at load of page
     setUpTabs();
 
     })
@@ -41,7 +37,7 @@ setUpTabs = () => {
 
 const url = 'https://wizard-world-api.herokuapp.com/Houses'
 
-fetch(url)
+fetch(url) //I made fetch happen
 .then(res => {
   return res.json()
 })
@@ -55,35 +51,37 @@ console.error("Nahhhh", error)
 
 // ----------------------------------------------- ^ Grabbing API ^---------------------------------------------------------------------
 
-// -----------------------------------------------Grabbing House info ---------------------------------------------------------------------
+// -----------------------------------------------Crest Images ---------------------------------------------------------------------
+
 let crests = [
   {
     crestname: "Gryffindor",
     crestImage: "https://m.media-amazon.com/images/I/71qheAe+f6L._AC_SX679_.jpg",
   },
-
+  
   {
     crestname: "Ravenclaw",
     crestImage: "https://m.media-amazon.com/images/I/61iys32RuAL._AC_SX679_.jpg",
   },
-
+  
   {
     crestname: "Huffelpuff",
     crestImage: "https://img.fruugo.com/product/0/50/46790500_max.jpg",
   },
-
+  
   {
     crestname: "Slytherin",
     crestImage: "https://m.media-amazon.com/images/I/71jTE5obH-L._AC_SX679_.jpg",
   }
 ]
-  
+// -----------------------------------------------^ Crest Images ^ ---------------------------------------------------------------------
 
+
+// -----------------------------------------------Grabbing House info ---------------------------------------------------------------------
 const houseContainer = document.querySelectorAll(".tabs__content");
 // console.log(document.querySelectorAll(`[data-tab]`))
 function displayHouse(arr) {
-  crests.forEach((crests, i) => {
-    // for (let i = 0; i < arr.length; i++) {
+  crests.forEach((crests, i) => { //changed to grab crests
       let htmlTemplate = `
       <div class="houseName">
       <h1>${arr[i].name}</h1>
@@ -92,35 +90,9 @@ function displayHouse(arr) {
       <p>House ghost: ${arr[i].ghost}</p>
       <p>Founder: ${arr[i].founder}</p>
       <p>Signature Traits: ${arr[i].traits[0, 3].name}, ${arr[i].traits[0, 2].name}, and ${arr[i].traits[0, 4].name}</p>
-      <img src = ${crests.crestImage}>
+      <img class = "allCrests" src = ${crests.crestImage}> 
       </div>`;
       document.querySelector(`[data-tab="${i}"]`).innerHTML = htmlTemplate
       // }
     })
 }
-
-
-//   arr.forEach((house) => {
-//     // console.log('house')
-//     let htmlTemplate = `
-//       <div class="houseName">
-//         <h1>${house.name}</h1>
-//         <p>House Colors: ${house.houseColours}</p>
-//         <p>Mascot: ${house.animal}</p>
-//         <p>House ghost: ${house.ghost}</p>
-//         <p>Founder: ${house.founder}</p>
-//         <p>Signature Traits: ${house.traits[0,3].name}, ${house.traits[0,2].name}, and ${house.traits[0,4].name}</p>
-//       </div>`
-//     ;
-
-//     houseContainer[i].insertAdjacentHTML("beforeend", htmlTemplate)
-//   });
-//   // for (let i = 1; i <= 5; i++){
-//   //   console.log(houseContainer[i])
-//   //   ;
-//   // }
-
-
-
-
-
