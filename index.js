279fcd19cbedc22d31b59b5b2aee18fac5de3afa
Matthew Holcombe <matthewholcombe@Matthews-MacBook-Pro-8.local>
2022-09-30 12:13:49 -0400

    add finishing touches

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ setUpTabs = () => {
       const tabsContainer = sideBar.parentElement; //div tabs
       const tabsNumber = button.dataset.forTab; //data-for-tab in button class
       const tabToActivate = tabsContainer.querySelector(`.tabs__content[data-tab="${tabsNumber}"]`); //tab content
-
+      // console.log(tabToActivate)
+      console.log(tabsContainer)
       // console.log(sideBar);
       // console.log(tabsContainer);
       // console.log(tabsNumber);
@@ -24,7 +25,7 @@ setUpTabs = () => {
       });
 
       button.classList.add('tabs__button--active');
-      // console.log(tabToActivate)
+      console.log(tabToActivate)
       tabToActivate.classList.add('tabs__content--active');
 
     })
@@ -45,36 +46,58 @@ fetch(url)
   return res.json()
 })
 .then(res => {
-  console.log('Caput Draconis', res)
+  // console.log('Caput Draconis', res)
   displayHouse(res);
 })
 .catch(error => {
 console.error("Nahhhh", error)
 })
-  
+
 // ----------------------------------------------- ^ Grabbing API ^---------------------------------------------------------------------
 
 // -----------------------------------------------Grabbing House info ---------------------------------------------------------------------
-  
+let crests = [
+  {
+    crestname: "Gryffindor",
+    crestImage: "https://m.media-amazon.com/images/I/71qheAe+f6L._AC_SX679_.jpg",
+  },
 
+  {
+    crestname: "Ravenclaw",
+    crestImage: "https://m.media-amazon.com/images/I/61iys32RuAL._AC_SX679_.jpg",
+  },
+
+  {
+    crestname: "Huffelpuff",
+    crestImage: "https://img.fruugo.com/product/0/50/46790500_max.jpg",
+  },
+
+  {
+    crestname: "Slytherin",
+    crestImage: "https://m.media-amazon.com/images/I/71jTE5obH-L._AC_SX679_.jpg",
+  }
+]
+  
 
 const houseContainer = document.querySelectorAll(".tabs__content");
 // console.log(document.querySelectorAll(`[data-tab]`))
 function displayHouse(arr) {
-  for (let i = 0; i < arr.length; i++){
-
-    let htmlTemplate = `
-       <div class="houseName">
-         <h1>${arr[i].name}</h1>
-         <p>House Colors: ${arr[i].houseColours}</p>
-         <p>Mascot: ${arr[i].animal}</p>
-        <p>House ghost: ${arr[i].ghost}</p>
-        <p>Founder: ${arr[i].founder}</p>
-        <p>Signature Traits: ${arr[i].traits[0, 3].name}, ${arr[i].traits[0, 2].name}, and ${arr[i].traits[0, 4].name}</p>
-        </div>`;
- 
-         document.querySelector(`[data-tab="${i}"]`).innerHTML = htmlTemplate
-  }
+  crests.forEach((crests, i) => {
+    // for (let i = 0; i < arr.length; i++) {
+      let htmlTemplate = `
+      <div class="houseName">
+      <h1>${arr[i].name}</h1>
+      <p>House Colors: ${arr[i].houseColours}</p>
+      <p>Mascot: ${arr[i].animal}</p>
+      <p>House ghost: ${arr[i].ghost}</p>
+      <p>Founder: ${arr[i].founder}</p>
+      <p>Signature Traits: ${arr[i].traits[0, 3].name}, ${arr[i].traits[0, 2].name}, and ${arr[i].traits[0, 4].name}</p>
+      <img src = ${crests.crestImage}>
+      </div>`;
+      document.querySelector(`[data-tab="${i}"]`).innerHTML = htmlTemplate
+      // }
+    })
+}
 
 
 //   arr.forEach((house) => {
@@ -96,7 +119,7 @@ function displayHouse(arr) {
 //   //   console.log(houseContainer[i])
 //   //   ;
 //   // }
-}
+
 
 
 
